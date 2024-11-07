e303066c48773f04625b1ccb7f2a4e2b78a4fb5d
Maksim Karpenko <ozuzuandvduvatel@gmail.com>
2024-11-07 16:48:16 +0200

    fix

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -8,12 +8,16 @@ import {
 import "./App.css";
 import HomePage from "../../pages/HomePage/HomePage";
 import MoviesPage from "../../pages/MoviesPage/MoviesPage";
+
 import Navigation from "../Navigation/Navigation";
-import { useEffect, useState } from "react";
+import { lazy, Suspense, useEffect, useState } from "react";
 import getMovies from "../../getPopularMovies";
-import MovieDetailsPage from "../../pages/MovieDetailsPage/MovieDetailsPage";
-import MovieCast from "../MovieCast/MovieCast";
-import MovieReviews from "../MovieReviews/MovieReviews";
+
+const MovieCast = lazy(() => import("../MovieCast/MovieCast"));
+const MovieReviews = lazy(() => import("../MovieReviews/MovieReviews"));
+const MovieDetailsPage = lazy(() =>
+  import("../../pages/MovieDetailsPage/MovieDetailsPage")
+);
 import NotFoundPage from "../../pages/NotFoundPage/NotFoundPage";
 
 function App() {
@@ -69,26 +73,27 @@ function App() {
   return (
     <>
       <Navigation />
+      <Suspense fallback={<div>Loading page...</div>}>
+        <Routes>
+          <Route
+            path="/"
+            element={<HomePage location={location} listMovie={topListMovie} />}
+          />
+          <Route
+            path="/movies"
+            element={<MoviesPage onSubmit={onSubmit} listMovie={searchMovie} />}
+          />
 
-      <Routes>
-        <Route
-          path="/"
-          element={<HomePage location={location} listMovie={topListMovie} />}
-        />
-        <Route
-          path="/movies"
-          element={<MoviesPage onSubmit={onSubmit} listMovie={searchMovie} />}
-        />
-
-        <Route
-          path="/movies/:movieId/"
-          element={<MovieDetailsPage query={query} />}
-        >
-          <Route path="cast" element={<MovieCast />} />
-          <Route path="reviews" element={<MovieReviews />} />
-        </Route>
-        <Route path="*" element={<NotFoundPage />} />
-      </Routes>
+          <Route
+            path="/movies/:movieId/"
+            element={<MovieDetailsPage query={query} />}
+          >
+            <Route path="cast" element={<MovieCast />} />
+            <Route path="reviews" element={<MovieReviews />} />
+          </Route>
+          <Route path="*" element={<NotFoundPage />} />
+        </Routes>
+      </Suspense>
     </>
   );
 }
